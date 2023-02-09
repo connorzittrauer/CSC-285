c99310b124dbd3ee1b6a50dc8febea9688d90118
connor <zittrauerconnor@gmail.com>
2023-02-09 11:15:14 -0600

    updated prologs//reformated

--- Singleton/src/main/java/org/example/AccountNumberGenerator.java
+++ Singleton/src/main/java/org/example/AccountNumberGenerator.java
@@ -1,28 +1,45 @@
+/*  Name: Connor Zittrauer
+ *  Course: CSC 285
+ *  Project: Singleton
+ *  File Name: AccountNumberGenerator.java
+ */
+
+/* This class implements the Singleton pattern.
+ * It can only return at most a single instance of the class,
+ * thus adhering to the Singleton principle. The purpose of this
+ * class is to return a new account number on each call of its instance.
+ */
+
 package org.example;
+
 public class AccountNumberGenerator
 {
     private static AccountNumberGenerator accountNumberGeneratorInstance;
     private int accountNo = 0;
+
+    /*
+     * Private constructor so that multiple instances of the class cannot be
+     * instantiated outside the class.
+     */
     private AccountNumberGenerator() {}
 
     /*
-     * Returns an instance of this class.
-     * Provides Lazy-loading so that an instance
-     * is only instantiated if there isn't already
-     * an existing instance.
+     * Returns an instance of this class. Provides Lazy-loading so that an instance
+     * is only instantiated if there isn't already an existing instance. If there is already
+     * an existing instance, then that instance is returned.
      */
-    public static AccountNumberGenerator getInstance() {
-        if (accountNumberGeneratorInstance == null) {
+    public static AccountNumberGenerator getInstance()
+    {
+        if (accountNumberGeneratorInstance == null)
+        {
             accountNumberGeneratorInstance = new AccountNumberGenerator();
         }
         return accountNumberGeneratorInstance;
     }
+
     /*
-    * This method increments the accountNo variable
-    * by one on each call. It is marked as 'synchronized'
-    * so that one thread at a time can execute this method
-    */
-    public synchronized int getNextNumber() {
-        return accountNo++;
-    }
+     * This method increments the accountNo variable by one on each call.
+     * It is marked as 'synchronized' so that only one thread at a time can execute this method.
+     */
+    public synchronized int getNextNumber() { return accountNo++; }
 }
--- Singleton/src/main/java/org/example/Driver.java
+++ Singleton/src/main/java/org/example/Driver.java
@@ -1,15 +1,23 @@
+/*  Name: Connor Zittrauer
+ *  Course: CSC 285
+ *  Project: Singleton
+ *  File Name: Driver.java
+ */
+
+/* This is the driver class for the Singleton design pattern. */
+
 package org.example;
-// a change
+
 public class Driver
 {
     public static void main(String[] args)
     {
         AccountNumberGenerator accountNumberGenerator = AccountNumberGenerator.getInstance();
 
-        // Verifies the getNextNumber Method
-        for (int i = 0; i <= 15; i++) {
+        // Verifies the getNextNumber method
+        for (int i = 0; i <= 15; i++)
+        {
             System.out.print(accountNumberGenerator.getNextNumber() + " ");
         }
-
     }
 }
